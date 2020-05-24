0f086fbb1b48dcdee69028521cde8076b8a93d07
Franka van Jaarsveld <frankav@student.uia.no>
2020-05-24 16:30:38 +0200

    CAM estimations for positional changes and acceleration not yet for directional changes

---
+++ estimateCAMs.py
@@ -0,0 +1,39 @@
+import math
+
+
+# Returns the number of CAMs for this vehicle
+def vehicle_cams(fps, vehicle, frame_amount, positionList, velocityList):
+    # We must keep track of which index of data we must compare to
+    # The framerate of the data determines which index we must look at for
+    # the next possible CAM
+    prev_cam = 0
+    minimum_wait = math.ceil(100/(1000/fps))
+    maximum_wait = math.ceil(1000/(1000/fps))
+    cams = 1
+
+    i = prev_cam + minimum_wait
+
+    while i < frame_amount:
+        if cam_conditions_are_met(prev_cam, i, positionList, velocityList) or (i >= prev_cam + maximum_wait):
+            # The CAM conditions are met or the maximum_wait has been reached
+            cams += 1
+            prev_cam = i
+            i = prev_cam + minimum_wait
+        else:
+            # The CAM conditions are not met, and the maximum_wait has not been reached
+            i += 1
+
+    print("Vehicle " + str(vehicle) + " has " + str(frame_amount) + " frames and " + str(cams) + " cams based on velocity data.")
+    return cams
+
+
+def cam_conditions_are_met(old, new, positions, velocities):
+    return change(positions[old], positions[new]) >= 4 \
+           or change(velocities[old], velocities[new]) >= 0.5 \
+           # or change(directions[old], directions[new]) >= 4
+
+
+def change(old, new):
+    dx = abs(old[0] - new[0])
+    dy = abs(old[1] - new[1])
+    return math.sqrt(pow(dx, 2) + pow(dy, 2))
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import csv
 from data import *
 import os
+from estimateCAMs import *
 
-# TRACK DATA INDEXES
+# GENERAL DATA STRUCTURE INFO
+FPS = 25
+
+# TRACK DATA (highD recordingMeta.csv)
+METAFILE = "_recordingMeta.csv"
 ID = 0
 NUM_VEHICLES = 10
 
-# VEHICLE DATA INDEXES (PER TRACK)
+# VEHICLE DATA (PER TRACK) (highD tracksMeta.csv)
+TRACKMETAFILE = "_tracksMeta.csv"
+FRAMES = 5
 DIRECTION = 7
 DISTANCE = 8
 AVG_VELOCITY = 11
 
-# FRAME DATA INDEXES (PER VEHICLE)
+# FRAME DATA (PER VEHICLE) (highD tracks.csv)
+TRACKFILE = "_tracks.csv"
 VEHICLE_ID = 1
 X_POSITION = 2
 Y_POSITION = 3
@@ -23,21 +31,81 @@ DHW = 12  # distance headway
 THW = 13  # time headway
 
 
-# Read data about one track as specified by the path
-def read_track(path):
-    with open(path + '_recordingMeta.csv') as metadata:
-        has_header = csv.Sniffer().has_header(metadata.read(1024))
+# Read all the data about one recording as specified by the path
+def read_recording(path):
+    vehicle_amount = None
+    frame_amounts = []
+    position_dict = {}
+    velocity_dict = {}
+    sniffer = csv.Sniffer()
+    cams = 0
+
+    # Get the number of vehicles per recording
+    with open(path + METAFILE) as metadata:
+        # Remove the header (variable names row) of the metadata of the recording
+        has_header = sniffer.has_header(metadata.read(1024))
         metadata.seek(0)
 
-        metaReader = csv.reader(metadata)
+        reader = csv.reader(metadata)
         if has_header:
-            next(metaReader)  # Don't need the rows with the variables names
+            next(reader)
 
-        for row in metaReader:
+        # View that meta data of this recording
+        for row in reader:
             print("Recording " + row[ID] + " has " + row[NUM_VEHICLES] + " vehicles.")
-            # Get the CAM prediction per vehicle in the track
-            # for vehicle_id in row[NUM_VEHICLES]:
-            #     #read_
+            vehicle_amount = int(row[NUM_VEHICLES])
+
+    # Get the number of frames per vehicle
+    with open(path + TRACKMETAFILE) as trackmeta:
+        # Remove the header (variable names row) of the trackmeta data of the recording
+        has_header = sniffer.has_header(trackmeta.read(1024))
+        trackmeta.seek(0)
+
+        reader = csv.reader(trackmeta)
+        if has_header:
+            next(reader)
+
+        # Store the number of frames for each vehicle in the recording
+        for row in reader:
+            frames = int(row[FRAMES])
+            frame_amounts.append(frames)
+
+    # Fill the vehicle dictionaries
+    with open(path + TRACKFILE) as trackdata:
+        # Remove the header (variable names row) of the trackdata of the recording
+        has_header = sniffer.has_header(trackdata.read(2048))
+        trackdata.seek(0)
+
+        reader = csv.reader(trackdata)
+        if has_header:
+            next(reader)
+
+        for row in reader:
+            vehicle = int(row[VEHICLE_ID])
+
+            # Fill in the vehicle position dictionary
+            position = (float(row[X_POSITION]), float(row[Y_POSITION]))
+            if vehicle in position_dict:
+                values = position_dict[vehicle]
+                values.append(position)
+            else:
+                values = [position]
+            position_dict[vehicle] = values
+
+            # Fill in the vehicle velocity dictionary
+            velocity = (float(row[X_VELOCITY]),float(row[Y_VELOCITY]))
+            if vehicle in velocity_dict:
+                values = velocity_dict[vehicle]
+                values.append(velocity)
+            else:
+                values = [velocity]
+            velocity_dict[vehicle] = values
+
+    # Get the CAM prediction per vehicle in the track
+    vehicle_id = 1
+    while vehicle_id <= vehicle_amount:
+        cams += vehicle_cams(FPS, vehicle_id, frame_amounts[vehicle_id - 1], position_dict[vehicle_id], velocity_dict[vehicle_id])
+        vehicle_id += 1
 
 
 # Read each track in the data
@@ -45,10 +113,16 @@ if __name__ == "__main__":
     track_num = 1
     path = 'data/01'
 
-    while os.path.exists(path + '_tracks.csv'):
-        read_track(path)
-        track_num += 1
-        if track_num < 10:
-            path = 'data/0' + str(track_num)
-        else:
-            path = 'data/' + str(track_num)
+    # For testing purposes (only look at recording 1):
+    read_recording(path)
+
+    # Read code:
+
+    # while os.path.exists(path + '_tracks.csv'):
+    #     read_track(path)
+    #     track_num += 1
+    #     if track_num < 10:
+    #         path = 'data/0' + str(track_num)
+    #     else:
+    #         path = 'data/' + str(track_num)
+
