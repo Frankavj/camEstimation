4cd735e31eff3a90f5994591e067f89b1e2577ab
Franka van Jaarsveld <frankav@student.uia.no>
2020-05-27 12:53:31 +0200

    macro params nu ook getest

--- tests/test_macroscopic_params.py
+++ tests/test_macroscopic_params.py
@@ -3,4 +3,22 @@ from macroscopicParams import *
 
 
 class TestMacroParams(TestCase):
-    # TODO
+    def setUp(self):
+        self.vehicle_amount = 4
+        self.duration = 4/25  # 4 frames
+        self.headway_dict = {1: [100, 120, 80, 100],
+                             2: [120, 110, 80, 90],
+                             3: [50, 50, 50, 50],
+                             4: [55, 50, 45, 50]}
+        self.avg_speeds = [35, 33, 27, 25]
+
+    def test_calculate_macro_params(self):
+        flow, density, speed = calculate_macroscopic_params(self.vehicle_amount,
+                                                            self.duration,
+                                                            self.headway_dict,
+                                                            self.avg_speeds)
+        self.assertEqual(flow, 25)
+        self.assertEqual(density, round(1 / 75, 4))
+        self.assertEqual(speed, 30)
+
+
